6e9ddc1b408951bb340351dedbb2711cb108ab75
swasthikg73 <swasthikg32@gmail.com>
2025-12-11 11:32:49 +0530

    Nav bar and Home pages added

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
+      <Link to={"/"}>
+        <img className="w-32 sm:w-44" src={assets.logo} alt="" />
+      </Link>
+      <button className="bg-zinc-800 text-white flex  item-center gap-4 px-6 py-2 text-sm rounded-full cursor-pointer">
+        Get started{" "}
+        <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
