1b38cd1895daba383021bb8a2c023b3573a5bfc1
swasthikg73 <swasthikg32@gmail.com>
2025-12-12 09:24:14 +0530

    Hero Section Addded

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -6,29 +6,36 @@ const HeroSection = () => {
     <div className="flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-4 mt-10 lg:px-44 sm:mt-20 ">
       {/*------------- Left side --------------------*/}
       <div className="">
-        <h1>
-          Remove the <br />
-          <span>bakground</span> from <br />
+        <h1 className="text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-700 leading-tight">
+          Remove the <br className="max-sm:hidden" />
+          <span className="bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent">
+            bakground
+          </span>{" "}
+          from <br className="max-sm:hidden" />
           images for free.
         </h1>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> illo
-          odio libero praesentium et accusamus voluptates in, ad laboriosam
-          tenetur assumenda?
+        <p className="my-6 text-[15px] text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          <br className="max-sm:hidden" /> illo odio libero praesentium et
+          accusamus voluptates in, ad laboriosam tenetur assumenda?
         </p>
 
         <div>
           <input type="file" name="" id="upload1" hidden />
-          <label htmlFor="upload1">
-            <img src={assets.upload_btn_icon} alt="" />
-            <p>Upload your image</p>
+          <label
+            className="inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700"
+            htmlFor="upload1">
+            <img width={20} src={assets.upload_btn_icon} alt="" />
+            <p className="text-white text-sm ">Upload your image</p>
           </label>
         </div>
       </div>
 
       {/*---------------------- Right Side ----------------*/}
-      <div></div>
+      <div className="w-full max-w-md">
+        <img src={assets.header_img} alt="" />
+      </div>
     </div>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
+import Steps from "../components/Steps";
+import BgSlider from "../components/BgSlider";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <Steps />
+      <BgSlider />
     </div>
   );
 };
