3ea14a7c8b5192c3951bc1d2e73c51bd3ec7563a
swasthikg73 <swasthikg32@gmail.com>
2025-12-14 11:48:37 +0530

    Navbar updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
+import { useClerk, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const { isSignedIn, user } = useUser();
+
   return (
     <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
       <Link to={"/"}>
         <img className="w-32 sm:w-44" src={assets.logo} alt="" />
       </Link>
-      <button className="bg-zinc-800 text-white flex  item-center gap-4 px-6 py-2 text-sm rounded-full cursor-pointer">
+      <button
+        onClick={() => openSignIn({})}
+        className="bg-zinc-800 text-white flex  item-center gap-4 px-6 py-2 text-sm rounded-full cursor-pointer">
         Get started{" "}
         <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
       </button>
