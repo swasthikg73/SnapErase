8b1974fae5ab2a1f28483254cba2e8b9229eb648
swasthikg73 <swasthikg32@gmail.com>
2025-12-14 11:44:21 +0530

    Footer added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { assets } from "../assets/assets";
+const Footer = () => {
+  return (
+    <div className=" flex flex-col md:flex-row items-center gap-4 justify-between px-4 lg:px-44 py-3">
+      <img width={150} src={assets.logo} alt="" />
+      <p className="text-gray-600">
+        Copyright @SnapClean | All right reserved.
+      </p>
+      <div className="flex gap-3">
+        <img width={40} src={assets.facebook_icon} alt="" />
+        <img width={40} src={assets.twitter_icon} alt="" />
+        <img width={40} src={assets.google_plus_icon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </BrowserRouter>
 );
