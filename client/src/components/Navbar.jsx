1ae4f3928d69ab5d35b943c8068074a17f54a120
swasthikg73 <swasthikg32@gmail.com>
2025-12-15 11:18:48 +0530

    Clerk code added in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
-import { useClerk, useUser } from "@clerk/clerk-react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const { openSignIn } = useClerk();
@@ -12,12 +12,18 @@ const Navbar = () => {
       <Link to={"/"}>
         <img className="w-32 sm:w-44" src={assets.logo} alt="" />
       </Link>
-      <button
-        onClick={() => openSignIn({})}
-        className="bg-zinc-800 text-white flex  item-center gap-4 px-6 py-2 text-sm rounded-full cursor-pointer">
-        Get started{" "}
-        <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
-      </button>
+      {isSignedIn ? (
+        <div>
+          <UserButton />
+        </div>
+      ) : (
+        <button
+          onClick={() => openSignIn({})}
+          className="bg-zinc-800 text-white flex  item-center gap-4 px-6 py-2 text-sm rounded-full cursor-pointer">
+          Get started{" "}
+          <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
+        </button>
+      )}
     </div>
   );
 };
