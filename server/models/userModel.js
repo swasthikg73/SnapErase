5d34f6dd9a1c1e29b1552142ee7032be0e6ed188
swasthikg73 <swasthikg32@gmail.com>
2025-12-16 13:31:57 +0530

    fix: import deleted from userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import userModel from "../models/userModel.js";
 
 const userSchema = new mongoose.Schema({
   clerkId: { type: String, require: true, unique: true },
