7bb50a79a4fdb45488674f22869d8b14e5e28468
swasthikg73 <swasthikg32@gmail.com>
2025-12-16 12:36:50 +0530

    fix: import path

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { clerkWebhooks } from "../controllers/userController";
+import { clerkWebhooks } from "../controllers/userController.js";
 const userRouter = express.Router();
 
 userRouter.post("/webhooks", clerkWebhooks);
