47b2584b3b31f93b8a4ab39dc953386981c0389d
swasthikg73 <swasthikg32@gmail.com>
2025-12-17 11:00:20 +0530

    Auth Middleware added

---
+++ server/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+    console.log("From header :", req.headers);
+
+    if (!token) {
+      return res.json({
+        success: false,
+        message: "Not authorized login Again",
+      });
+    }
+
+    const token_decode = jwt.decode(token);
+    console.log(" Decoded token :", token_decode);
+    req.body.clerkId = token_decode.clerkId;
+    next();
+  } catch (error) {
+    console.log(error.message);
+    return res.json({ success: false, message: error.message });
+  }
+};
+export default authMiddleware;
