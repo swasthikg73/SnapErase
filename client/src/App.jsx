c27ecd9b233e458f8268b880f4b62d9ca205d645
swasthikg73 <swasthikg32@gmail.com>
2025-12-18 10:24:09 +0530

    Frontend changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Result from "./pages/Result";
 import BuyCredit from "./pages/Buy";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-50">
+      <ToastContainer position="bottom-right" />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,22 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from "./context/AppContext.jsx";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
-import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </ClerkProvider>
   </BrowserRouter>
 );
