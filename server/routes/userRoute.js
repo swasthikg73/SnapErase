474fdec065e2a1547e377c8801946403e1b01998
swasthikg73 <swasthikg32@gmail.com>
2025-12-20 12:26:29 +0530

    Transaction model added

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { clerkWebhooks, userCredits } from "../controllers/userController.js";
+import {
+  clerkWebhooks,
+  paymentRazorpay,
+  userCredits,
+  verifyPayment,
+} from "../controllers/userController.js";
 const userRouter = express.Router();
 
 import authMiddleware from "../middlewares/auth.js";
@@ -7,5 +12,9 @@ import authMiddleware from "../middlewares/auth.js";
 userRouter.post("/webhooks", clerkWebhooks);
 
 userRouter.get("/credits", authMiddleware, userCredits);
+
+userRouter.post("/pay-razor", authMiddleware, paymentRazorpay);
+
+userRouter.post("/verify-payment", authMiddleware, verifyPayment);
 
 export default userRouter;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
 import userRouter from "./routes/userRoute.js";
+import imageRouter from "./routes/imageRoute.js";
 
 //App config
 const PORT = process.env.PORT || 4000;
@@ -16,6 +17,7 @@ app.use(cors());
 //API routes
 
 app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
 app.get("/", (req, res) => res.send("API working"));
 
 app.listen(PORT, () => {
